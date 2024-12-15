576b62310b57e0ff48220f9aeae2888e98ed90a6
pm-235779 <148884916+pm-235779@users.noreply.github.com>
2024-12-16 01:52:56 +0530

    Update VerifyToken.js

--- backend/middlewares/VerifyToken.js
+++ backend/middlewares/VerifyToken.js
@@ -2,8 +2,8 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 dotenv.config();
 export const verifyToken = (req, res, next) => {
-    const token = req.cookies.token;
-   if (!token) {
+    const token = req.cookies.token 
+       if (!token) {
        return res.status(401).json({sucess: false, message: "Unauthorized"});    
    }
    try {
