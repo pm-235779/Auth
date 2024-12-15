7c977ecff9b065fe044323f822729e5ae034c10e
pm-235779 <148884916+pm-235779@users.noreply.github.com>
2024-12-16 01:56:47 +0530

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import { signup, login, logout, verifyEmail , forgotPassword , resetPassword,checkAuth} from '../controllers/auth.controller.js';
 import { verifyToken } from '../middlewares/VerifyToken.js';
+import { 
+    addContact,
+    getContacts,
+    updateContact,  
+    deleteContact
+} from '../controllers/contact.controller.js';
 const router = express.Router();
 
 router.get('/check-Auth', verifyToken, checkAuth);
@@ -16,5 +22,13 @@ router.post('/logout', logout);
 router.post('/reset-password', forgotPassword);
 
 router.post('/reset-password/:token', resetPassword);
+
+router.post('/contacts', verifyToken, addContact);           // Add a new contact
+
+router.get('/contacts', verifyToken, getContacts);           // Get all contacts
+
+router.put('/contacts/:id', verifyToken, updateContact);     // Update a contact
+
+router.delete('/contacts/:id', verifyToken, deleteContact);  // Delete a contact
 
 export default router;
